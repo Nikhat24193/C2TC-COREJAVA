1b03292c79b74869feb0f245e23e3cbd00ad8421
DELL-PC <DELL-PC@DESKTOP-QE7QH2M>
2022-03-17 22:30:05 +0530

    Java Operators

---
+++ DAY12/src/DataType_Demo/Demo_Datatypes.java
@@ -0,0 +1,16 @@
+package DataType_Demo;
+
+public class Demo_Datatypes {
+
+	public static void main(String[] args) 
+	{
+
+		//char varl = '\u00A7';
+		//char varl = '\u00A5';
+		char varl = '\u00B4';
+		
+		System.out.println("The char value is "+varl);
+		
+	}
+
+}
---
+++ DAY12/src/DataType_Demo/Ternary_Operator.java
@@ -0,0 +1,16 @@
+package DataType_Demo;
+
+public class Ternary_Operator {
+
+	public static void main(String[] args) 
+	{
+
+		int x = 10;
+		
+		x = (10 == x) ? 1 : 0;
+		
+		System.out.println(x);
+
+	}
+
+}
