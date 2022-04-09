d0c242b779045b181785b95d5b1525d4edf078b5
DELL-PC <DELL-PC@DESKTOP-QE7QH2M>
2022-04-09 17:32:11 +0530

    Generics

---
+++ DAY24/src/Generics/GD.java
@@ -0,0 +1,25 @@
+package Generics;
+class Demo <T,U>
+{
+	T obj1;
+	U obj2;
+	Demo(T obj1,U obj2)
+	{
+		this.obj1=obj1;
+		this.obj2=obj2;
+	}
+	public void print ()
+	{
+	System.out.println(obj1);
+	System.out.println(obj2);
+	}
+}
+public class GD{
+
+	public static void main(String[] args) {
+		
+     Demo <String,Integer> ob= new Demo<String,Integer>("nikhat",24693);
+     ob.print();
+
+}
+}
