92025c49ce4557ae658f74712c45aea3b0f25ac2
DELL-PC <DELL-PC@DESKTOP-QE7QH2M>
2022-04-09 17:33:58 +0530

    Lambda Expression

---
+++ DAY26/src/LambdaExpression/Lambdaforeach.java
@@ -0,0 +1,21 @@
+package LambdaExpression;
+
+
+	import java.util.LinkedList;
+
+	public class Lambdaforeach {
+
+		public static void main(String[] args) 
+		{
+		LinkedList<String> ll=new LinkedList<String>();
+		ll.add("amjad");
+		ll.add("imtiyaz");
+		ll.add("farhat");
+		ll.add("nikhat");
+		ll.forEach((n)->{System.out.println(n);});
+		
+
+		}
+
+	}
+
