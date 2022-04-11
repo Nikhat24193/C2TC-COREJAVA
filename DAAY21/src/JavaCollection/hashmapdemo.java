1b13f4e2b136f95f9ea0d0d38012b5eceab3c856
DELL-PC <DELL-PC@DESKTOP-QE7QH2M>
2022-04-11 16:29:27 +0530

    java collection

---
+++ DAAY21/src/JavaCollection/hashmapdemo.java
@@ -0,0 +1,15 @@
+package JavaCollection;
+
+import java.util.HashMap;
+
+public class hashmapdemo {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> hmap=new HashMap<Integer,String>();
+		hmap.put(1, "Kopek");
+		hmap.put(2, "Misaal");
+		System.out.println(hmap);
+
+	}
+
+}
